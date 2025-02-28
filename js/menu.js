96e9244870c1ba52c4d79fce4b2cd53d485c2c46
DRLKs <davidmunvalle@gmail.com>
2025-02-28 20:05:27 +0100

    Initial Commit

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+document.getElementById('play-btn').addEventListener('click', function() {
+    window.location.href = '../index.html';
+});
+document.getElementById('settings-btn').addEventListener('click', function() {
+    alert('Ajustes no implementados aún.');
+});
+document.getElementById('ranking-btn').addEventListener('click', function() {
+    alert('Ranking no implementado aún.');
+});
