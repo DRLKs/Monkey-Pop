879e57ac7b1c131c2e28b3635d7b5ab12788502f
DRLKs <davidmunvalle@gmail.com>
2025-03-01 11:04:44 +0100

    Añado apartado ajustes
    Controles no se actualizan correctamente
    ** IDEAS **
    - Como es un juego de ordenador, quiero que los controles salgan, FLOW cruceta para poder cambiarlos

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@ document.getElementById('play-btn').addEventListener('click', function() {
     window.location.href = 'juego.html';
 });
 document.getElementById('settings-btn').addEventListener('click', function() {
-    alert('Ajustes no implementados aún.');
+    window.location.href = 'ajustes.html'
 });
 document.getElementById('ranking-btn').addEventListener('click', function() {
     alert('Ranking no implementado aún.');
