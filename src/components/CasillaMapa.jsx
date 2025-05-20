5676a3f64463460d81afbf5126f11da78b7c870b
DRLKs <davidmunvalle@gmail.com>
2025-05-20 23:05:56 +0200

    Fase beta de verdad

--- src/components/CasillaMapa.jsx
+++ src/components/CasillaMapa.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import '../styles/CasillaMapa.css'
 
+
+import Globo from './Globo'
+import MonoMapa from './MonoMapa'
+import globoExplotado from '../assets/images/globos/globoExplotado.webp'
+
 /**
  * 
  * @param {*} estado: Estado de la casilla, puede ser 'default', 'agua', 'camino', 'agua_cesped1', 'agua_cesped2', 'tierra_cesped1', 'tierra_cesped2', 'selected'. El selected es un estado del 'default'
  * @returns 
  */
-export const CasillaMapa = ({ estado = 'default', index, actualizarMapa }) => {
+export const CasillaMapa = ({ estado = 'default', index, actualizarMapa, globos, monos, explotaGloboCasilla  }) => {
 
     const manejarInteraccion = () => {
         actualizarMapa(index)
@@ -17,8 +22,21 @@ export const CasillaMapa = ({ estado = 'default', index, actualizarMapa }) => {
             onClick={manejarInteraccion} 
             onTouchStart={manejarInteraccion}
             className={`casilla-mapa casilla-${estado}`}
-            data-index={index}
         >
+
+        {globos.map(globo => (
+            <Globo 
+            key={globo.id}
+            health={globo.health}
+            />
+        ))}
+        {monos.map(mono => (
+            <MonoMapa 
+            key={mono.id}
+            tipo={mono.tipo}
+            />
+        ))}
+        {explotaGloboCasilla && <img className='globo-explotado' src={globoExplotado} />}
         </div>
 
     )
